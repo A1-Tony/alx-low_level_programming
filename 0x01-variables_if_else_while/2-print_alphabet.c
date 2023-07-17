fc9e1b92fd6a381bcb76e1f3f729fb2ac5718dfe
Antony <antonykinyanjui279@gmail.com>
2023-07-17 16:18:33 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: (0) success
+ */
+int main(void)
+{
+/* your code goes there */
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar ('\n');
+return (0);
+}
