dfc7147bc6d2384006dbafb8762e6f49c7e8149e
Antony <antonykinyanjui279@gmail.com>
2023-07-31 18:51:30 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,7 +5,7 @@
  * @n: function copies
  * @src: byte from memory area
  * @dest: to memory area
- * return: a pointer to @dest
+ * Return: a pointer to @dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
